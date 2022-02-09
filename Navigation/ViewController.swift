efa4e6fdd4704b4c00b8fe7ef77fce7f1f15a8d7
max_egorov <max.egorov@sureprep.com>
2022-02-08 23:31:21 -0800

    Initial Commit

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Max Egorov on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
